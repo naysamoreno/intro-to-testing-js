a7d2c45c6bce0ca78a396c5162498033522d3be1
Naysa Moreno <naysaymoreno@gmail.com>
2021-04-05 21:14:31 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,9 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function sayHello(name) {
+    // return "Hello, " + name + "!";
+}
+function addOne(input) {
+    return 1;
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,13 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+})
+describe("addOne", function() {
+    it('should be a defined function', function() {
+        expect(typeof addOne).toBe("function");
+    });
+});
